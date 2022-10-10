d22f14b70c53bb9b9e08c28edbaf88d1f36545b3
shashwatshukla <shashwat.link@gmail.com>
2022-10-10 22:22:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 import io
 
 st.set_page_config(page_title='Job Master', layout='wide')
-
+st.info('This app over comes limitation of excel pivot where the user cannot change data from pivot table directly. \n Excel file is read in pandas and written in aggrid')
 def to_excel(df) -> bytes:
     output = io.BytesIO()
     writer = pd.ExcelWriter(output, engine="xlsxwriter")

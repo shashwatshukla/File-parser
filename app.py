01afc1d001530e335fe1cd11be6ced29d9533f74
shashwatshukla <shashwat.link@gmail.com>
2022-10-10 22:53:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 import io
 
 st.set_page_config(page_title='Job Master', layout='wide')
-st.info('This app over comes limitation of excel pivot where the user cannot change data from pivot table directly.  \n
-Excel file is read in pandas and rendered using aggrid where the data can be pivoted and edited in the cell.  \n
-The edited data is then written back to dataframe which can be rexported back to excel')
+st.info('This app over comes limitation of excel pivot where the user cannot change data from pivot table directly.  \nExcel file is read in pandas and rendered using aggrid where the data can be pivoted and edited in the cell.  \nThe edited data is then written back to dataframe which can be rexported back to excel')
 def to_excel(df) -> bytes:
     output = io.BytesIO()
     writer = pd.ExcelWriter(output, engine="xlsxwriter")
